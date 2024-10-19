65409982013e7159f82c364abc964baa2af55d10
mquinto18 <quintom53@gmail.com>
2024-10-19 11:02:05 +0800

    new repo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -64,10 +64,7 @@ class HomeController extends Controller
         return view('dashboard', compact('totalUsers', 'totalAppointment', 'totalAmount', 'totalCompleted', 'appointments','appointmentsAll', 'monthlyEarnings', 'statusCounts'));
     }
     
-    
-    
-    
-    
+
 
     public function profileSettings(){
         return view('components.profile');
