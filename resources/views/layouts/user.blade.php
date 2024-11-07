ee436db156f1a2ba65d298c93fa450b7d013974f
Matthew Quinto <quintom53@gmail.com>
2024-11-08 00:18:03 +0800

    new repo

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+@notifyCss
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -170,6 +171,8 @@
         </main>
     </div>
 
+    <x-notify::notify />
+    @notifyJs
 </body>
 
 </html>
